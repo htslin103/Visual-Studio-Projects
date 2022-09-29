a4c420bc51acb74fdddbbcf20d6c7e2b269db3da
Helen L <htslin103@gmail.com>
2022-09-28 20:37:35 -0600

    Added the beginning of the practice project

---
+++ WPFPracticeProject/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFPracticeProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnAddName.FontSize = 50;
+            btnRemoveName.Content = "Booga";
+            string dbConnStr = ConfigurationManager.ConnectionStrings["DefaultConnectionString"].ConnectionString;
+        }
+
+        private void btnAddName_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("added name");
+        }
+
+        private void btnRemoveName_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("deleted name");
+        }
+    }
+}
